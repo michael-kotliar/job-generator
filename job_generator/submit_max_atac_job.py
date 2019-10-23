685902d5d172f52f0f352984d62dd790a10f2f7b
Michael Kotliar <misha.kotliar@gmail.com>
2019-10-23 16:38:31 -0400

    Submits jobs from sra annotation file if run with --sra

--- job_generator/submit_max_atac_job.py
+++ job_generator/submit_max_atac_job.py
@@ -11,6 +11,8 @@ INDEX_COLUMNS = ["Experiment accession", "Biological replicate(s)", "Paired end"
 EXP_COLUMNS = ["Experiment accession", "Biological replicate(s)"]
 FILTER_COLUMN = "Run type"
 FILTER_VALUE = "paired-ended"
+INDEX_COLUMNS_SRA = ["exp_id", "technical_rep"]
+EXP_COLUMNS_SRA = ["exp_id"]
 CWD = os.getcwd()
 
 
@@ -24,6 +26,7 @@ def arg_parser():
     general_parser.add_argument("-g", "--genome",      help="Path to genome FASTA file",                     required=True)
     general_parser.add_argument("-n", "--number",      help="Limit number of experiments to submit",         type=int)
     general_parser.add_argument("-o", "--output",      help="Path to be used as output_folder in job files", required=True)
+    general_parser.add_argument("-s", "--sra",         help="Use metadata file with SRA identifiers",        action="store_true")
     return general_parser
 
 
@@ -70,6 +73,90 @@ def properly_paired(first_files, second_files):
         row_count = len(second_files.index)
         second_files = second_files.iloc[pd.Index(second_files["File accession"]).get_indexer(first_files["Paired with"])]
         return (equal(first_files, second_files) and len(second_files.index) == row_count)
+
+
+def get_metadata_sra(metadata_file):
+    raw_data = pd.read_table(metadata_file, index_col=INDEX_COLUMNS_SRA, comment='#')
+    return raw_data.sort_index()
+
+
+def extract_sra(srr_files, run_id):
+    first_filename = run_id + "_R1.fastq.gz"
+    second_filename = run_id + "_R2.fastq.gz"
+    first_combined_filepath = os.path.join(CWD, first_filename)
+    second_combined_filepath = os.path.join(CWD, second_filename)
+
+    if os.path.isfile(first_combined_filepath) or os.path.isfile(second_combined_filepath):
+        raise Exception(f"""File {first_combined_filepath} or {second_combined_filepath} already exists""")
+
+    for srr_id in srr_files:
+        params = f"""docker run --rm -ti -v {CWD}:/tmp/ biowardrobe2/sratoolkit:v2.8.2-1 fastq-dump --split-3 --gzip {srr_id} &&
+                     cat {srr_id}_1.fastq.gz >> {first_filename} &&
+                     cat {srr_id}_2.fastq.gz >> {second_filename} &&
+                     rm {srr_id}_1.fastq.gz {srr_id}_2.fastq.gz"""
+        env = os.environ.copy()
+        print("\n  Run", params)
+        try:
+            subprocess.run(params, env=env, shell=True, check=True)
+        except Exception as err:
+            try:
+                os.remove(first_combined_filepath)
+                os.remove(second_combined_filepath)
+                os.remove(os.path.join(CWD, f"""{srr_id}_1.fastq.gz"""))
+                os.remove(os.path.join(CWD, f"""{srr_id}_2.fastq.gz"""))
+            except Exception as err:
+                pass
+            raise err
+    return first_combined_filepath, second_combined_filepath
+
+
+def submit_jobs_sra (args, metadata):
+    count = 0
+    for exp_idx, exp_data in metadata.groupby(level=EXP_COLUMNS_SRA):
+        if args.number and count >= args.number:
+            break
+        print("\n\n\nProcess experiment:\n\n  Experiment accession -", exp_idx)
+        srr_files = exp_data["srr_id"].tolist()
+        count = count + 1
+        try:
+            print("\n  SRR: ", srr_files)
+            run_id = exp_idx
+            first_combined, second_combined = extract_sra(srr_files, run_id)
+            job_template = {
+                "job": {
+                    "fastq_file_1": {
+                        "class": "File",
+                        "location": first_combined
+                    },
+                    "fastq_file_2": {
+                        "class": "File",
+                        "location": second_combined
+                    },
+                    "indices_folder": {
+                        "class": "Directory",
+                        "location": args.indices
+                    },
+                    "blacklisted_regions_bed": {
+                        "class": "File",
+                        "location": args.blacklisted
+                    },
+                    "chrom_length_file": {
+                        "class": "File",
+                        "location": args.chromlength
+                    },
+                    "genome_size": "2.7e9",
+                    "genome_fasta_file": {
+                        "class": "File",
+                        "location": args.genome
+                    },
+                    "output_folder": os.path.join(args.output, run_id),
+                    "threads": 4
+                }
+            }
+            print(dumps(job_template, indent = 4))
+            trigger_dag(job_template, run_id, args.dag)
+        except Exception as err:
+            print("\n  Failed to submit job:", err)
 
 
 def submit_jobs (args, metadata):
@@ -130,10 +217,13 @@ def main(argsl=None):
     if argsl is None:
         argsl = sys.argv[1:]
     args,_ = arg_parser().parse_known_args(argsl)
-    args = normalize_args(args, ["dag", "number"])
-
-    metadata = get_metadata(args.metadata)
-    submit_jobs(args, metadata)
+    args = normalize_args(args, ["dag", "number", "sra"])
+    if args.sra:
+        metadata = get_metadata_sra(args.metadata)
+        submit_jobs_sra(args, metadata)
+    else:
+        metadata = get_metadata(args.metadata)
+        submit_jobs(args, metadata)
 
 
 if __name__ == "__main__":
